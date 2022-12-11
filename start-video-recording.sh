6077c051b957559fcdada6012547908ac62c7b0f
Julio <IceXPR@users.noreply.github.com>
2022-12-10 21:39:00 -0500

    add photo recording

--- start-video-recording.sh
+++ start-video-recording.sh
@@ -3,5 +3,7 @@
 while true
 do
  mkdir -p public/bird-videos
- raspivid -o - -t 60000 | ffmpeg -r 8 -i - -y -vcodec copy public/bird-videos/bird-video-`date +%H%M`.mp4
+ mkdir -p public/bird-photos
+ raspistill -o public/bird-photos/bird-photo-`date +%H%M`.jpg
+ raspivid -o - -t 59000 | ffmpeg -r 8 -i - -y -vcodec copy public/bird-videos/bird-video-`date +%H%M`.mp4
 done
