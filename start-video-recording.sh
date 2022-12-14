cabfd7a1023173578531c0a6ff4b668d34f8ce91
Julio <IceXPR@users.noreply.github.com>
2022-12-14 00:00:33 -0500

    clear old files

--- start-video-recording.sh
+++ start-video-recording.sh
@@ -4,6 +4,8 @@ while true
 do
  mkdir -p public/bird-videos
  mkdir -p public/bird-photos
+ find public/bird-videos/ -name '*.*' -type f -mmin +1440 -delete
+ find public/bird-photos/ -name '*.*' -type f -mmin +1440 -delete
  raspistill -o public/bird-photos/bird-photo-`date +%H%M`.jpg
  raspivid -o - -t 59000 | ffmpeg -r 8 -i - -y -vcodec copy public/bird-videos/bird-video-`date +%H%M`.mp4
 done
