3db05067b55d84f22a58f70d97e33dd23f0b7b15
Julio <IceXPR@users.noreply.github.com>
2022-12-17 20:26:31 -0500

    update snapshot time to be closer to the starting of the snapshot

--- start-video-recording.sh
+++ start-video-recording.sh
@@ -6,8 +6,9 @@ do
  mkdir -p public/bird-photos
  find public/bird-videos/ -name '*.*' -type f -mmin +1440 -delete
  find public/bird-photos/ -name '*.*' -type f -mmin +1440 -delete
- raspistill -o public/bird-photos/bird-photo-`date +%H%M`.jpg
 
+ export snapshot=`date +%H%M`
+ raspistill -o public/bird-photos/bird-photo-$snapshot.jpg
  raspivid -o video.h264 -t 59000
- MP4Box -add video.h264 public/bird-videos/bird-video-`date +%H%M`.mp4
+ MP4Box -add video.h264 public/bird-videos/bird-video-$snapshot.mp4
 done
